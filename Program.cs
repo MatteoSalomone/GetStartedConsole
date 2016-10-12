8f1caef06dc107d182542a95dd548a9f01cb8c74
Matteo Salomone <matteo.salomone@hotmail.com>
2016-10-12 10:56:03 +0200

    First commit: basic console app with VS git ignore

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        /*
+        * tutorial founded here:
+        * https://docs.microsoft.com/en-us/dotnet/articles/core/tutorials/using-with-xplat-cli
+        */
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
