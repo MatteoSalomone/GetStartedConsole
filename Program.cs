2f5bb1cec2636a434d53d85efb284d1ce2320002
Matteo Salomone <matteo.salomone@hotmail.com>
2016-10-12 11:57:01 +0200

    Fibonacci first implementation

--- Program.cs
+++ Program.cs
@@ -1,16 +1,36 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace ConsoleApplication
 {
+    /*
+    * tutorial founded here:
+    * https://docs.microsoft.com/en-us/dotnet/articles/core/tutorials/using-with-xplat-cli
+    */
     public class Program
     {
-        /*
-        * tutorial founded here:
-        * https://docs.microsoft.com/en-us/dotnet/articles/core/tutorials/using-with-xplat-cli
-        */
+        public static int FibonacciNumber(int n) 
+        {
+            int a = 0;
+            int b = 1;
+            int tmp;
+
+            for (int i = 0; i < n; i++)
+            {
+                tmp = a;
+                a = b;
+                b += tmp;
+            }
+
+            return a;
+        }
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Fibonacci Numbers 1-15:");
+
+            for (int i = 0; i < 15; i++)
+            {
+                WriteLine($"{i+1}: {FibonacciNumber(i)}");
+            }
         }
     }
 }
